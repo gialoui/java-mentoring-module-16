5619a7034e57ab2d5acf70d8d59211eeddcbf77f
KhangNDD <khang_nguyen1@epam.com>
2022-12-26 18:33:36 +0700

    Init project

---
+++ src/main/java/learn/java/module16/Module16Application.java
@@ -0,0 +1,13 @@
+package learn.java.module16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module16Application.class, args);
+	}
+
+}
---
+++ src/test/java/learn/java/module16/Module16ApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.java.module16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
